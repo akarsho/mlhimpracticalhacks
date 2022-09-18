bc9e15486411e6eb5bd676a50ff39121d05054eb
Akarsh Ojha <64885810+akarsho@users.noreply.github.com>
2022-09-17 22:58:08 -0700

    Added database push, hashing, and other functionality
    Additionally, included some element changes. The program can now push tasks to the databse and retrieve it (to send to the Repeater object).

--- MiseryOrganizer.js
+++ MiseryOrganizer.js
@@ -1,4 +1,7 @@
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+import {returnHashedObject} from 'backend/handleBackend.jsw'
+import {saveDB} from 'backend/handleBackend.jsw'
+import wixLocation from 'wix-location';
 
 $w.onReady(function () {
 
@@ -13,6 +16,7 @@ $w.onReady(function () {
 	// Click "Run", or Preview your site, to execute your code
 
 	$w("#box1").hide()
+	$w("#box2").hide()
 
 });
 
@@ -25,5 +29,47 @@ export function button1_click(event) {
 	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
 	// Add your code for this event here: 
 	$w('#button1').disable();
-	$w("#box1").show('fade', { duration: 550, delay: 0 });
+	$w("#box2").show('fade', { duration: 550, delay: 0 });
+}
+
+/**
+*	Adds an event handler that runs when the element is clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function button3_click(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+	$w("#box1").show('fade', { duration: 190, delay: 0 });
+}
+
+/**
+*	Adds an event handler that runs when the element is clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function button2_click(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+	$w("#box1").hide('fade', { duration: 190, delay: 0 });
+}
+
+/**
+*	Adds an event handler that runs when the element is clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function button4_click(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+	$w("#box1").hide('fade', { duration: 190, delay: 0 });
+	let name = $w("#input1").value; let type = $w("#dropdown1").value; let date = $w("#datePicker1").value; let importance = $w('#dropdown2').value;
+	console.log(name, type, date, importance);
+	if(name.length > 0 && type.length > 0 && importance.length > 0)
+	{
+		returnHashedObject(name, type, date, importance).then( item => {
+			saveDB(item);
+		});
+		wixLocation.to("/redirect");
+	}
 }
