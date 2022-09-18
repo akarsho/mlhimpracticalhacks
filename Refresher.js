cca023a4336a0b3308a01d13799fecca4ebb8647
Akarsh Ojha <64885810+akarsho@users.noreply.github.com>
2022-09-17 23:01:00 -0700

    Backend update + redirection
    Update to include handling object creation for tasks, database pushing and pulling.
    
    Also included redirection page.

---
+++ Refresher.js
@@ -0,0 +1,5 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+import wixLocation from 'wix-location';
+$w.onReady(function () {
+	wixLocation.to("/morg");
+});
